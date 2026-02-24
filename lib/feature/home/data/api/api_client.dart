94bc6fc0347376cd0aa880afb498eb0b689843ec
unknown <tadroussamir2001@gmail.com>
2026-02-23 16:48:29 -0800

    first commit

--- lib/feature/home/data/api/api_client.dart
+++ lib/feature/home/data/api/api_client.dart
@@ -1,7 +1,8 @@
-// import '../models/proudct_model_dto.dart';
-//
-// class ApiClient {
-//   Future<List<ProudctModelDto>> getProducts() async {
-//     return await [ProudctModelDto(), ProudctModelDto(), ProudctModelDto()];
-//   }
-// }
+import '../models/proudct_model_dto.dart';
+
+class ApiClient {
+    Future<List<ProudctModelDto>> getProducts() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return  [ProudctModelDto(description:"dfv" ,imageUrl:"dfv" ,name:"dfvdf" ,price:12 )];
+  }
+}
--- lib/feature/home/data/data_source/remote_data_source.dart
+++ lib/feature/home/data/data_source/remote_data_source.dart
@@ -1,10 +1,14 @@
 import 'package:elevate_course/feature/home/data/data_source/remote_data_source.contract.dart';
 import 'package:elevate_course/feature/home/data/models/proudct_model_dto.dart';
 
+import '../api/api_client.dart';
+
 class RemoteDataSource implements RemoteDataSourceContract{
+   RemoteDataSource (this.apiClient);
+  final ApiClient apiClient;
   @override
   Future<List<ProudctModelDto>> getProducts() {
-    return Future.value(<ProudctModelDto>[]);
+    return apiClient.getProducts();
 
   }
 
